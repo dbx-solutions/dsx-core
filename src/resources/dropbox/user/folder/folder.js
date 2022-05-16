b4360da374f6777a9c656dd75653c9e6c504c6f5
Hussam Hebbo <hussam@dropbox.com>
2022-05-16 11:15:19 +0200

    made auto rename folder false by default

--- src/resources/dropbox/user/folder/folder.js
+++ src/resources/dropbox/user/folder/folder.js
@@ -1,11 +1,11 @@
-export async function createFolder(dbx, path, autoRename) {
+export async function createFolder(dbx, path, autoRename = false) {
 	const response = await dbx.filesCreateFolderV2({path: path, autorename: autoRename});
 	const createdFolder = response.result;
 
 	return createdFolder;
 }
 
-export async function createFolderBatch(dbx, paths, autoRename) {
+export async function createFolderBatch(dbx, paths, autoRename = false) {
 	const response = await dbx.filesCreateFolderBatch({paths: paths, autorename: autoRename})
 	const createdFolders = response.result;
 
